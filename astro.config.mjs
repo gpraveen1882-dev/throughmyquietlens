fcade28eb1fbed36e10909d3fdb038cd05455d5a
gpraveen1882 <gpraveen1882@gmail.com>
2026-07-26 12:08:27 +0800

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,3 +0,0 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-export default defineConfig({ site: 'https://throughmyquietlens.com', integrations: [sitemap()] });
